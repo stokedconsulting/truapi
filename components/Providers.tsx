ad423e6c71d76eab59513a3ab25848e688d78f2a
Van Hohenheim <0xhohenheim@gmail.com>
2025-03-18 03:47:45 +0530

    feat: onchainkit migration

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -3,30 +3,24 @@
 import type { ReactNode } from 'react';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
 import { base, baseSepolia } from 'wagmi/chains';
-import ReactQueryClientProvider from './ReactQueryClientProvider';
 import { ClerkProvider } from '@clerk/nextjs';
 import { UserProvider } from '@/contexts/user.context';
-import { WagmiProvider } from 'wagmi';
-import { wagmiAdapter } from '@/config/reown';
 
 export function Providers({ children }: { children: ReactNode }) {
     return (
-        <ReactQueryClientProvider>
-            <WagmiProvider config={wagmiAdapter.wagmiConfig}>
-                <OnchainKitProvider
-                    apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-                    chain={process.env.NEXT_APP_ENV === "production"
-                        ? base
-                        : baseSepolia}
-                    config={{ appearance: { theme: 'light' } }}
-                >
-                    <ClerkProvider>
-                        <UserProvider>
-                            {children}
-                        </UserProvider>
-                    </ClerkProvider>
-                </OnchainKitProvider>
-            </WagmiProvider>
-        </ReactQueryClientProvider>
+        // Wagmi + ReactQuery Providers created by OnchainKitProvider
+        <OnchainKitProvider
+            apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+            chain={process.env.NEXT_APP_ENV === "production"
+                ? base
+                : baseSepolia}
+            config={{ appearance: { theme: 'light' }, wallet: { display: 'modal' } }}
+        >
+            <ClerkProvider>
+                <UserProvider>
+                    {children}
+                </UserProvider>
+            </ClerkProvider>
+        </OnchainKitProvider>
     );
 }
